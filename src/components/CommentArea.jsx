e62e23c6f3dfd6842523fe399d36a98701eb944c
METAORI <nico97.mecca@gmail.com>
2024-08-27 15:30:59 +0200

    first commit

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,82 @@
+import CommentList from './CommentList'
+import AddComment from './AddComment'
+import Loading from './Loading'
+import Error from './Error'
+import { useState,useEffect } from 'react'
+
+const CommentArea = (props) => {
+
+  const[comments,setComments]= useState([])
+  const[isLoading,setIsLoading]= useState(false)
+  const[isError,setIsError]= useState(false)
+
+  // componentDidMount = async () => {
+  //   try {
+  //     let response = await fetch(
+  //       'https://striveschool-api.herokuapp.com/api/comments/' +
+  //         this.props.asin,
+  //       {
+  //         headers: {
+  //           Authorization:
+  //             'Bearer inserisci-qui-il-tuo-token',
+  //         },
+  //       }
+  //     )
+  //     console.log(response)
+  //     if (response.ok) {
+  //       let comments = await response.json()
+  //       this.setState({ comments: comments, isLoading: false, isError: false })
+  //     } else {
+  //       console.log('error')
+  //       this.setState({ isLoading: false, isError: true })
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //     this.setState({ isLoading: false, isError: true })
+  //   }
+  // }
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        setIsLoading(true);
+        let response = await fetch(
+          'https://striveschool-api.herokuapp.com/api/comments/' + props.asin,
+          {
+            headers: {
+              Authorization: 'Bearer inserisci-qui-il-tuo-token',
+            },
+          }
+        );
+        console.log(response);
+        if (response.ok) {
+          let comments = await response.json();
+          setComments(comments);
+          setIsLoading(false);
+          setIsError(false);
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+        }
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    };
+  
+    fetchComments();
+  }, [props.asin]);
+
+
+    return (
+      <div className="text-center">
+        {isLoading && <Loading />}
+        {isError && <Error />}
+        <AddComment asin={props.asin} />
+        <CommentList commentsToShow={comments} />
+      </div>
+    )
+  }
+
+
+export default CommentArea
